499f9ef8d4f6ccb47c4452dc7f2dacdaae259241
Bridge Tools <robot@bridgetools.io>
2021-11-30 21:34:43 +0200

    Avatar component

---
+++ design-system/src/components/Avatar/index.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import styled from "styled-components";
+
+/* Avatar
+ *
+ * TODO: add component description
+ */
+const Avatar = (props) => {
+  return <StyledAvatar>Avatar</StyledAvatar>;
+};
+
+export const StyledAvatar = styled.div``;
+
+export default Avatar;
--- design-system/src/index.js
+++ design-system/src/index.js
@@ -1,13 +1,14 @@
 /*
  * This is an index file for your library.
  * It's being updated automatically by add-component script
- * Don't edit it directly, your chages will be overwritten.
+ * You can add tokens/components reexport, it will be preserved at next run, but not anything else.
  */
 
 export { default as GlobalStyle } from "./global.js";
 export { colors, typography, spacing } from "./tokens";
 
 
+export { default as Avatar } from "./components/Avatar";
 export { default as Button } from "./components/Button";
 export { default as Carousel } from "./components/Carousel";
 export { default as Icon } from "./components/Icon";
